ddda8be735c5f3f21702bc2df34f2555b0454711
unknown <Akansha0211>
2026-02-20 00:53:33 +0530

    one to one mapping of token and userid

--- app/src/main/java/org/example/entities/RefreshToken.java
+++ app/src/main/java/org/example/entities/RefreshToken.java
@@ -1,4 +1,26 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tokens")
 public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String token;
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "user_id")
+    private UserInfo userInfo;
 }

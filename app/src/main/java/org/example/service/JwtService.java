6d2f7719047e589e173e194ccd31ea015cef5e24
unknown <Akansha0211>
2026-02-20 14:40:41 +0530

    JwtService is ready

--- app/src/main/java/org/example/service/JwtService.java
+++ app/src/main/java/org/example/service/JwtService.java
@@ -2,11 +2,14 @@ package org.example.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 /*
@@ -36,6 +39,34 @@ public class JwtService {
     }
     public Date extractExpiration(String token){
         return extractClaim(token, Claims:: getExpiration);
+    }
+
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    /*
+    * this method checks whether token is valid or not
+    * based on userdetails coming from Database , gets username matches it from username in token
+    *  and checks if token not expired*/
+    public Boolean validateToken (String token, UserDetails userDetails){
+        final String username = extractUsername(token); // extrcating user name from token
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    /*
+    * Big picture
+    * creating token(THIS METHOD BELOW) --> extracting claims(Creating JWT using parser(i.e builder)
+    * Claims having several method like extracting username, extracting expiration : Claims::getExpiration ( USING IT AS ExtractClaim method --> where we are JWT parser, using token creatig JWT  ans then applying method on it i.e apply()
+    * --> returns apply(claims) which can be with return type String or Date
+    * */
+    private String createToken(Map<String,Object> claims, String username){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() * 1000 * 60 * 1))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256);
     }
 
     private Claims extractAllClaims(String token){

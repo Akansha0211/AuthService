a8608beaaf6d9152903c3403222f1e222c6e46e2
unknown <Akansha0211>
2026-04-07 00:04:19 +0530

    JWTAuthFilter done

--- app/src/main/java/org/example/config/JwtAuthFilter.java
+++ app/src/main/java/org/example/config/JwtAuthFilter.java
@@ -1,5 +1,60 @@
 package org.example.config;
 
-public class JwtAuthFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.example.service.JwtService;
+import org.example.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
+/*
+* same req, same user , same token)multiple times--> considered as a single req
+* --> only one time JWT Filter applied to it
+* */
+@Component
+@AllArgsConstructor
+@Data
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    private final JwtService jwtService;
+    @Autowired
+    private final UserDetailsServiceImpl userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String userName = null;
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            token = authHeader.substring(7);
+            userName = jwtService.extractUsername(token);
+        }
+        /*
+        * If username is not null from the token
+        * and if SecurityContext is not set for the user which means first time this user is requesting
+        * --> then we will set the context, and move ahead in the FilterChain of our spring security config
+        *  In our case there is no other filter so request will go to the Controller
+        * */
+        if(userName != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+            if(jwtService.validateToken(token, userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication((authenticationToken));
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
